4c8ff9cbd90d906ca56f039bba4516ea71e55719
Mohsen Bazmi <55460935+Mohsen-Bazmi@users.noreply.github.com>
2022-04-06 23:41:04 +0430

    refactor: Refactor the Contact record

--- Models/Contact.cs
+++ Models/Contact.cs
@@ -5,6 +5,6 @@ using System.ComponentModel.DataAnnotations;
 public record Contact(string Name)
 {
     [Display(Name = "Phone Number")] public string? PhoneNumber { get; set; }
-    [Display(Name = "Email")] public string? Email { get; set; }
-    [Display(Name = "Website")] public string? Website { get; set; }
+    public string? Email { get; set; }
+    public string? Website { get; set; }
 }
